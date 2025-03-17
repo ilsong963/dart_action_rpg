4b4d1aa296ca3b553f700f3c063d62fe2ab2ae7c
ilsong963 <ilsong963@naver.com>
2025-03-17 16:14:25 +0900

    init

--- bin/dart_action_rpg.dart
+++ bin/dart_action_rpg.dart
@@ -1,5 +1,1 @@
-import 'package:dart_action_rpg/dart_action_rpg.dart' as dart_action_rpg;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_action_rpg.calculate()}!');
-}
+void main() {}
