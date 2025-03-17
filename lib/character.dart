a7c15231f89a9486eb77ed227be4c8ca76190445
ilsong963 <ilsong963@naver.com>
2025-03-17 16:29:37 +0900

    character 초기 구조 셋팅

---
+++ lib/character.dart
@@ -0,0 +1,12 @@
+class Character {
+  String name;
+  int health;
+  int attack;
+  int defense;
+
+  Character(this.name, this.health, this.attack, this.defense);
+
+  void attackMonster(Monster monster) {}
+  void defend() {}
+  void showStatus() {}
+}
