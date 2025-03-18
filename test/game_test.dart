435741d0a587dfc0bbd7f9dd6ab58be723d26ce7
ilsong963 <ilsong963@naver.com>
2025-03-18 12:01:18 +0900

    test: 테스트 파일 생성

---
+++ test/game_test.dart
@@ -0,0 +1,10 @@
+import 'package:dart_action_rpg/game.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late Game game;
+
+  group("game 테스트", () {
+    setUp(() => game = Game());
+  });
+}
