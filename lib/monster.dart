0e972fe673dcabf184aaefbff031d74f5044a084
yooilsong <ilsong963@naver.com>
2025-03-19 22:20:59 +0900

    feat: defend에 따른 공격 로직 수정

--- lib/monster.dart
+++ lib/monster.dart
@@ -9,16 +9,30 @@ class Monster {
   int randAttackMax;
   int defense = 0;
 
-  Monster({required this.name, required this.health, required this.randAttackMax});
+  Monster({
+    required this.name,
+    required this.health,
+    required this.randAttackMax,
+  });
 
   void attackCharacter(Character character) {
-    int randDamge = Random().nextInt(randAttackMax + 1);
+    int randDamage = Random().nextInt(randAttackMax + 1);
+    int totalDamage = 0;
 
-    if (randDamge < character.defense) {
-      character.health -= character.defense;
-    } else {
-      character.health -= randDamge;
+    if (character.isDefend) {
+      print("${character.name}(이)가 $name의 공격을 막았습니다.");
+      character.isDefend = false;
+      return;
     }
+
+    if (randDamage < character.defense) {
+      totalDamage = character.defense;
+    } else {
+      totalDamage = randDamage;
+    }
+    character.health -= totalDamage;
+
+    print("$name(이)가 ${character.name}에게 $totalDamage의 데미지를 입혔습니다");
   }
 
   void showStatus() {
