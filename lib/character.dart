a44144eaea4a8bac475b588bd9417bcbc6f0fdb7
yooilsong <ilsong963@naver.com>
2025-03-19 22:13:34 +0900

    feat: defend 기능 추가

--- lib/character.dart
+++ lib/character.dart
@@ -5,14 +5,24 @@ class Character {
   int health;
   int attack;
   int defense;
+  bool isDefend = false;
 
-  Character({required this.name, required this.health, required this.attack, required this.defense});
+  Character({
+    required this.name,
+    required this.health,
+    required this.attack,
+    required this.defense,
+  });
 
   void attackMonster(Monster monster) {
+    print("$name(이)가 ${monster.name}에게 $attack의 데미지를 입혔습니다");
     monster.health -= attack;
   }
 
-  void defend() {}
+  void defend() {
+    isDefend = true;
+  }
+
   void showStatus() {
     print('$name - 체력: $health, 공격력: $attack, 방어력: $defense');
   }
