51e0d6835c202d87fe384ca03494d8fd655f655b
ilsong963 <ilsong963@naver.com>
2025-03-20 13:23:59 +0900

    feat: askLoop 파라미터 추가

--- lib/helper.dart
+++ lib/helper.dart
@@ -1,18 +1,13 @@
 import 'dart:io';
 
-dynamic askLoop({
-  required String question,
-  required String error,
-  required dynamic answer1,
-  required dynamic answer2,
-}) {
+dynamic askLoop({required String question, required String error, required dynamic answer1, required dynamic answer2, dynamic answer3, dynamic answer4}) {
   String? answer;
 
   while (true) {
     print(question);
     answer = stdin.readLineSync();
 
-    if (answer != null && (answer == answer1 || answer == answer2)) {
+    if (answer != null && (answer == answer1 || answer == answer2 || answer == answer3 || answer == answer4)) {
       return answer;
     }
     if (answer != null) {
