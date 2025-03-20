cf92a141fc64a91a87492215809468ac456cfc47
ilsong963 <ilsong963@naver.com>
2025-03-20 18:14:06 +0900

    test: useItem 테스트 코드 작성

--- test/character_test.dart
+++ test/character_test.dart
@@ -1,17 +1,41 @@
 import 'package:dart_action_rpg/character.dart';
+import 'package:dart_action_rpg/item.dart';
 import 'package:dart_action_rpg/monster.dart';
 import 'package:test/test.dart';
 
 void main() {
   late Character character;
   group("Character 테스트", () {
-    setUp(() => character = Character(name: "test", health: 100, attack: 10, defense: 10));
+    setUp(() => character = Character(name: "test", baseHealth: 100, baseAttack: 10, baseDefense: 10));
 
     test("attackMonster 테스트", () {
       Monster monster = Monster(name: 'testMonster', health: 10, randAttackMax: 10);
       character.attackMonster(monster);
 
       expect(monster.health, 0);
+    });
+
+    test("useItem 선택한 아이템이 삭제되는지 테스트", () {
+      character.itemList = [Item.hp, Item.attack, Item.defense];
+
+      character.useItem(2);
+
+      expect(character.itemList, [Item.hp, Item.defense]);
+    });
+
+    test("useItem 선택한 아이템효과 테스트", () {
+      character.itemList = [Item.hp, Item.attack, Item.defense];
+
+      character.useItem(1);
+
+      expect(character.health, 200);
+
+      character.useItem(1);
+
+      expect(character.attack, 20);
+      character.useItem(1);
+
+      expect(character.defense, 20);
     });
   });
 }
