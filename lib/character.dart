1cd1f365a9cdb1822f72387985cb6e7e864ee73a
ilsong963 <ilsong963@naver.com>
2025-03-20 18:19:44 +0900

    fix: item 관련 이전코드 제거

--- lib/character.dart
+++ lib/character.dart
@@ -1,3 +1,4 @@
+import 'package:dart_action_rpg/item.dart';
 import 'package:dart_action_rpg/monster.dart';
 
 class Character {
@@ -7,8 +8,13 @@ class Character {
   int defense;
   bool isDefend = false;
   bool hasItem = true;
+  List<Item> itemList = [Item.hp, Item.attack, Item.defense];
+  int baseAttack, baseDefense, baseHealth;
 
-  Character({required this.name, required this.health, required this.attack, required this.defense});
+  Character({required this.name, required this.baseHealth, required this.baseAttack, required this.baseDefense})
+    : attack = baseAttack,
+      defense = baseDefense,
+      health = baseHealth;
 
   void attackMonster(Monster monster) {
     print("$name(이)가 ${monster.name}에게 $attack의 데미지를 입혔습니다.\n");
@@ -19,17 +25,27 @@ class Character {
     isDefend = true;
   }
 
-  void useItem() {
-    hasItem = false;
-    _increaseAttackByTwo();
+  void useItem(int index) {
+    Item item = itemList.removeAt(index - 1);
+
+    switch (item) {
+      case Item.hp:
+        health *= 2;
+        break;
+      case Item.attack:
+        attack *= 2;
+        break;
+      case Item.defense:
+        defense *= 2;
+        break;
+    }
   }
 
-  void _increaseAttackByTwo() {
-    attack *= 2;
-  }
-
-  void resetAttack() {
-    attack = (attack / 2).toInt();
+  void resetStats() {
+    attack = baseAttack;
+    defense = baseDefense;
+    health = baseHealth;
+    print("아이템 효과 종료.");
   }
 
   void showStatus() {
